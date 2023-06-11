55dfe4bca57b7dbf82604178599a95499ef465b0
marcato10 <lucasmarcato10@hotmail.com>
2023-06-11 11:39:39 -0300

    FEATURE/FILTER_CARGO: CRUD de cursos está completo.

--- lib/AdmPages/Cursos.dart
+++ lib/AdmPages/Cursos.dart
@@ -55,31 +55,6 @@ import 'package:login_page/model/Values.dart';
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => DeletarCursoPage(loggedUser: loggedUser),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                  ),
-                  child: const Text(
-                    'Deletar Curso',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              SizedBox(
-                width: 200.0,
-                height: 60.0,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
                         builder: (context) => TodosCursosPage(loggedUser: loggedUser),
                       ),
                     );
@@ -122,7 +97,6 @@ import 'package:login_page/model/Values.dart';
       try{
         http.Response response = await http.post(URL,
             headers: {'Content-Type': 'application/json; charset=utf-8'}, body: jsonCurso);
-
         switch(response.statusCode){
           case 200:
             const snackBar = SnackBar(
@@ -159,7 +133,6 @@ import 'package:login_page/model/Values.dart';
       }
 
     }
-
     @override
     Widget build(BuildContext context) {
       return Scaffold(
@@ -167,7 +140,8 @@ import 'package:login_page/model/Values.dart';
           title: const Text('Criar Curso'),
         ),
         body: ListView(
-          children:[ Padding(
+          children:[
+            Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
@@ -225,70 +199,9 @@ import 'package:login_page/model/Values.dart';
     }
   }
 
-  class DeletarCursoPage extends StatelessWidget {
-    final User loggedUser;
-    const DeletarCursoPage({Key? key, required this.loggedUser}) : super(key: key);
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Deletar Curso'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              DropdownButton<String>(
-                items: const [
-                  DropdownMenuItem<String>(
-                    value: 'Curso 1',
-                    child: Text('Curso 1'),
-                  ),
-                  DropdownMenuItem<String>(
-                    value: 'Curso 2',
-                    child: Text('Curso 2'),
-                  ),
-                  DropdownMenuItem<String>(
-                    value: 'Curso 3',
-                    child: Text('Curso 3'),
-                  ),
-                ],
-                onChanged: (value) {
-                  // Lógica para selecionar o curso
-                },
-                hint: const Text('Selecione um curso'),
-              ),
-              const SizedBox(height: 16.0),
-              SizedBox(
-                width: double.infinity,
-                height: 60.0,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Lógica para deletar o curso
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                  ),
-                  child: const Text(
-                    'Deletar',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-  }
 
 class TodosCursosPage extends StatefulWidget {
   final User loggedUser;
-
   const TodosCursosPage({Key? key, required this.loggedUser}) : super(key: key);
 
   @override
@@ -297,6 +210,7 @@ class TodosCursosPage extends StatefulWidget {
 
 class _TodosCursosPageState extends State<TodosCursosPage> {
   List<Curso> cursoList = [];
+  bool isConfirmedShowed = false;
 
   @override
   void initState() {
@@ -322,6 +236,89 @@ class _TodosCursosPageState extends State<TodosCursosPage> {
     }
   }
 
+  void showConfirmationDialog(BuildContext context, Curso curso,User user) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmar exclusão'),
+          content: Text('Deseja excluir o curso "${curso.titulo}"?'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                deleteCurso(context, curso.id,user);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Confirmar'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+  void deleteCurso(BuildContext context,int id,User user) async{
+    String jsonUser = jsonEncode(user.toJson());
+    http.Response response = await http.delete(
+        Uri.parse('$mainURL/cursos/$id'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonUser);
+
+    try{
+      switch(response.statusCode){
+        case 200:
+          const snackBar = SnackBar(
+            content: Text('Curso excluído com sucesso.'),
+            duration: Duration(seconds: 2),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          setState(() {
+            getCursos();
+          });
+          break;
+
+        case 403:
+          const snackBar = SnackBar(
+            content: Text('Curso não excluído: Usuário sem permissão.'),
+            duration: Duration(seconds: 2),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        break;
+
+        case 409:
+          const snackBar = SnackBar(
+            content: Text('Curso não excluído: Está associado a um ou mais treinamentos.'),
+            duration: Duration(seconds: 2),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          break;
+
+        default:
+          const snackBar = SnackBar(
+            content: Text('Erro de Requisição: Verifique se os parâmetros foram passados corretamente.'),
+            duration: Duration(seconds: 2),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          break;
+      }
+    }catch(e){
+      if(e is SocketException){
+        const snackBar = SnackBar(
+          content: Text('Erro de conexão: Verifique sua conexão com o sistema.'),
+          duration: Duration(seconds: 2),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -333,11 +330,16 @@ class _TodosCursosPageState extends State<TodosCursosPage> {
         shrinkWrap: true,
         itemBuilder: (BuildContext context, int index) {
           Curso curso = cursoList[index];
-
           return ListTile(
             leading: Text('${curso.id}'),
             title: Text(curso.titulo),
             subtitle: Text(curso.descricao),
+            trailing: IconButton(
+              icon: const Icon(Icons.clear_outlined),
+              onPressed: (){
+                showConfirmationDialog(context,curso,widget.loggedUser);
+              },
+            ),
           );
         },
       ),
